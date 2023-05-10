32002e7a6f17dcf7f3d0fd2b9f52ca8f7b09e493
Shaken-Dan Bolatuly <shd.cs.ch@gmail.com>
2023-05-10 16:46:29 +0600

    In capture created a url link importer filestack

--- filesharer.py
+++ filesharer.py
@@ -1,3 +1,5 @@
+from filestack import Client
+
 class FileSharer:
     def __init__(self, filepath, api_key='AFLwFAOJSACkH1eBLP1Q0z'):
         self.api_key = api_key
--- main.py
+++ main.py
@@ -26,16 +26,25 @@ class CameraScreen(Screen):
             strftime() is method of time library used to create a unique name
         """
         current_time = time.strftime('%Y%m%d-%H%M%S')
-        filepath = f'image_files/{current_time}.png'
-        self.ids.camera.export_to_png(filepath)
+        # self makes filepath an attribute of CameraScreen Instance
+        self.filepath = f'image_files/{current_time}.png'
+        self.ids.camera.export_to_png(self.filepath)
 
         # Accessing manager attribute from Screen instance
         self.manager.current = 'image_screen'
-        self.manager.current_screen.ids.img.source = filepath
+        self.manager.current_screen.ids.img.source = self.filepath
+
 
 class ImageScreen(Screen):
-    pass
-
+    def create_link(self):
+        """ Access to filepath of CameraScreen class."""
+        file_path = App.get_running_app().root.ids.camera_screen.filepath
+        # Instance of FileSharer class
+        filesharer = FileSharer(filepath = file_path)
+        # returns an URL
+        url = filesharer.share()
+        # Get access to label and assigns Widget instance text attribute with url string address
+        self.ids.link.text = url
 
 class RootWidget(ScreenManager):
     pass
